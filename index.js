46f98fd805e5061704c5d38e41430cebe671c566
Thapasya-s <thapasyasajee@gmail.com>
2024-08-10 08:37:48 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// const name = 'me'
+// console.log(name,'this is')
+
+// const kalki = (actor)=>{
+//     console.log('called ',actor)
+// }
+// kalki("prabhas")
+
+// const timeoutClear = setTimeout(() =>{
+//     console.log('fgfwy')
+// } ,1000);
+
+// clearTimeout(timeoutClear)
+
+// const b = setInterval(() => {
+//     console.log("dxfgj")
+    
+// }, 1000);
+
+// clearInterval(b)
+
+// console.log(__dirname);
+// console.log(__filename)
+
+const rockys_gold = require('./newfile.js')
+console.log(rockys_gold,'ivide und')
---
+++ newfile.js
@@ -0,0 +1,5 @@
+const rocky = 'gold'
+const bahu = 'silver'
+
+module.exports = rocky
+module.exports = bahu
